7310b35c256627b79f1f2a31d82ad393d098cc3b
kkitetsu <b787kitetsu@gmail.com>
2024-04-04 09:02:23 +0900

    merge from dev

--- src/main/java/com/example/todo/controller/LibraryController.java
+++ src/main/java/com/example/todo/controller/LibraryController.java
@@ -1,12 +1,16 @@
 package com.example.todo.controller;
 
+import java.security.NoSuchAlgorithmException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.todo.forms.DongwookRequest;
+import com.example.todo.utils.HashGenerator;
 
 @Controller
 public class LibraryController {
@@ -18,10 +22,33 @@ public class LibraryController {
 		return "tutorial";
 	}
 	
-	@RequestMapping(value="/kitetsu" , method=RequestMethod.POST)
-	public String confirmLibrary(Model model,DongwookRequest don) {
-		String dongwook = don.getSuzaki();
-		model.addAttribute("kuzawa" , dongwook);
-		return "confirm";
+	@GetMapping(value="/login")
+	public String getLoginPage(Model model) {
+		return "/login";
 	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String doLogin(Model model, @RequestParam("id") String id,
+									   @RequestParam("pw") String pw) {
+		
+		String hashedPassword = "";
+		try {
+			hashedPassword = HashGenerator.generateHash(pw);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		// TODO: Handle login logic
+		System.out.println(hashedPassword);
+		
+		if (true) {
+			return "/login";
+		}
+		
+		return "/home";
+	}
+	
+	@GetMapping("/register")
+    public String getRegisterPage() {
+        return "/register";
+    }
 }

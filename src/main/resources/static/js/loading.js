e3b5a66c2e9ae439c6aecba3657158c40ca73797
Leedongwook24 <159088755+Leedongwook24@users.noreply.github.com>
2024-04-10 15:22:20 +0900

    loading display

---
+++ src/main/resources/static/js/loading.js
@@ -0,0 +1,7 @@
+/**
+ * @author Lee
+ * loading用 javasctipy
+ */
+ document.getElementById('small_size_loading').addEventListener('click', function() {
+    document.getElementById('loading').style.zIndex = '9999';
+});
